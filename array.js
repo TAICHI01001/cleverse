7ee1f597fabe5d745b7db698b6df04a5860dcd8a
TAICHI01001 <anothai.0978452316@gmail.com>
2023-05-23 22:01:01 +0700

    JS

--- array.js
+++ array.js
@@ -1,17 +1,46 @@
-const food = ["apple", "banana", "orange", 'watermelon']
-
-console.log(food)
-
-index = food.indexOf("mango")
-
-food.push("mango", "Kiwi", "durian")
-
-console.log(food)
-
-allFood = food.includes("orange")
 
 
-console.log(index)
-console.log(allFood)
+const a = ["a", "b", "c", "d"]
+const b = ["e", "f", "g", "i"]
+
+
+
+// const d = a.indexOf("d")
+// console.log(d);
+
+// const c = a.concat(b)
+// console.log(c);
+
+// a.push("k")
+// console.log(a)
+
+// a.unshift("o")
+// console.log(a)
+
+// a.pop()
+// console.log(a);
+
+// a.shift()
+// console.log(a);
+
+
+
+
+
+// const food = ["apple", "banana", "orange", 'watermelon']
+
+// console.log(food)
+
+// index = food.indexOf("mango")
+
+// food.push("mango", "Kiwi", "durian")
+
+// console.log(food)
+
+// allFood = food.includes("orange")
+
+
+// console.log(index)
+// console.log(allFood)
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,2 @@
-
-  // for (let i = 0; i < arr.length; i++) {
-  //   for (let j = 0; j < arr.length; j++) {
-  //     if (arr[i] < arr[j]) {
-  //       let tmp = arr[i];
-  //       arr[i] = arr[j];
-  //       arr[j] = tmp;
-  //     }
-  //   }
-  // }
-  // console.log(arr);
+a.push("k")
+console.log(a)
