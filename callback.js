0e84e5e4b9dcce4d4b51313ad5fe07e7366657c0
TAICHI01001 <anothai.0978452316@gmail.com>
2023-05-26 09:10:04 +0700

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,3 +1,16 @@
+
+// const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a + b) === 100
+
+// console.table(isEqualTo100(0, 100));
+// console.table(isEqualTo100(100, 0));
+// console.table(isEqualTo100(10, 0));
+// console.table(isEqualTo100(0, 10));
+// console.table(isEqualTo100(20, 80));
+// console.table(isEqualTo100(50, 50));
+
+
+
+
 // function cb() {
 //   return "Hello World!"
 // }
@@ -6,45 +19,45 @@
 // console.log("This is()", cb());
 
 
-const arr = [-5, -7, 5, 2, 8]
-function filter(arr, cb) {
-  const result = []
-  for (let i = 0; i < arr.length; i++) {
-    const array = arr[i]
-    if (cb(array)) {
-      result.push(arr[i])
-    }
-  }
-  return result;
-}
-function Pos(n) {
-  return n >= 0
-}
-function Neg(n) {
-  return n < 0
-}
-console.log(filter(arr, Pos))
-console.log(filter(arr, Neg))
+// const arr = [-5, -7, 5, 2, 8]
+// function filter(arr, cb) {
+//   const result = []
+//   for (let i = 0; i < arr.length; i++) {
+//     const array = arr[i]
+//     if (cb(array)) {
+//       result.push(arr[i])
+//     }
+//   }
+//   return result;
+// }
+// function Pos(n) {
+//   return n >= 0
+// }
+// function Neg(n) {
+//   return n < 0
+// }
+// console.log(filter(arr, Pos))
+// console.log(filter(arr, Neg))
 
-const a = [-10, 11, 12, 13, -1, -2, -3, -4]
+// const a = [-10, 11, 12, 13, -1, -2, -3, -4]
 
-function filter(a, b) {
-  Ans = []
-  for (let i = 0; i < a.length; i++) {
-    if (b(a[i])) {
-      Ans.push(a[i])
-    }
-  }
-  return Ans
-}
+// function filter(a, b) {
+//   Ans = []
+//   for (let i = 0; i < a.length; i++) {
+//     if (b(a[i])) {
+//       Ans.push(a[i])
+//     }
+//   }
+//   return Ans
+// }
 
-function add(x) {
-  // return x = "I like you"
-}
-function del(x) {
-  // return x = "I don't like you"
-}
-console.log(filter(a, add))
-console.log(filter(a, del))
+// function add(x) {
+//   // return x = "I like you"
+// }
+// function del(x) {
+//   // return x = "I don't like you"
+// }
+// console.log(filter(a, add))
+// console.log(filter(a, del))
 
 // console.log(filter([-5, -7, 5, 2, 8], Pos));
