cc0040f7f28e464f58788da424d6967075c2cb39
TAICHI01001 <anothai.0978452316@gmail.com>
2023-05-27 10:38:38 +0700

    JS

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
+const number = BigInt("98765432109876543210")
+const number2 = 2**53
+
+console.log(number);
+console.log(number2);
 
 
-function grade(score) {
-  if (score < 0 || score > 100) {
-    console.log("pleases input you score again");
-    return
-  }
 
 
 
-  if (score <= 49) {
-    console.log("grade F")
-  }
-  else if (score <= 59) {
-    console.log("grade D")
-  }
-  else if (score <= 69) {
-    console.log("grade C")
-  }
-  else if (score <= 79) {
-    console.log("grade B")
-  }
-  else if (score > 80 || score <= 100) {
-    console.log("grade A")
-  }
-  else {
-    console.log("input you again")
-  }
-}
-grade(81)
 
-grade = (score) => {
-  if (score <= 49) {
-    console.log("grade F")
-  }
-  else if (score <= 59) {
-    console.log("grade D")
-  }
-  else if (score <= 69) {
-    console.log("grade C")
-  }
-  else if (score <= 79) {
-    console.log("grade B")
-  }
-  else if (score > 80 || score <= 100) {
-    console.log("grade A")
-  }
-  else {
-    console.log("input you again")
-  }
-}
-grade(74)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function grade(score) {
+//   if (score < 0 || score > 100) {
+//     console.log("pleases input you score again");
+//     return
+//   }
+
+
+
+//   if (score <= 49) {
+//     console.log("grade F")
+//   }
+//   else if (score <= 59) {
+//     console.log("grade D")
+//   }
+//   else if (score <= 69) {
+//     console.log("grade C")
+//   }
+//   else if (score <= 79) {
+//     console.log("grade B")
+//   }
+//   else if (score > 80 || score <= 100) {
+//     console.log("grade A")
+//   }
+//   else {
+//     console.log("input you again")
+//   }
+// }
+// grade(81)
+
+// grade = (score) => {
+//   if (score <= 49) {
+//     console.log("grade F")
+//   }
+//   else if (score <= 59) {
+//     console.log("grade D")
+//   }
+//   else if (score <= 69) {
+//     console.log("grade C")
+//   }
+//   else if (score <= 79) {
+//     console.log("grade B")
+//   }
+//   else if (score > 80 || score <= 100) {
+//     console.log("grade A")
+//   }
+//   else {
+//     console.log("input you again")
+//   }
+// }
+// grade(74)
 
