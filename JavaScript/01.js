707274cd06a7db0587c66f7cc19d6cc8c11c6c3c
TAICHI01001 <anothai.0978452316@gmail.com>
2023-05-30 16:32:52 +0700

    Learn JS

--- JavaScript/01.js
+++ JavaScript/01.js
@@ -1,13 +1,6 @@
+
+
 function myFunction() {
   let x = document.getElementById("demo");
-  x.style.fontSize = "25px";
-  x.style.color = "red";
-  x.style.transition = "0.75s"
-  x.style.transform = "translate(180px)"
-}
-
-function youFunction() {
-  let x = document.getElementById("demo");
-  x.style.transition = "0.75s"
-  x.style.transform = "translate(0px)"
+  x.style.display = "block";
 }
--- JavaScript/02.js
+++
@@ -1,7 +0,0 @@
-const number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-function num() {
-  for (let i = 0; i < number.length; i++) {
-    sum = sum + number[i]
-  }
-  result = sum / number.length
-}
